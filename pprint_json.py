464d3632783d3d1fead2ce9d27bff9b30fbd1206
antondemkin <antondemkin@yandex.ru>
2017-07-20 16:38:11 +0300

    Added check if file exists and is a correct json file.

--- pprint_json.py
+++ pprint_json.py
@@ -11,9 +11,12 @@ def main():
     if len(sys.argv) > 1:
         path = sys.argv[1]
         if os.path.exists(path):
-            with open(path, encoding='cp1251') as file:
-                json_data = json.load(file)
+            try:
+                with open(path) as file:
+                    json_data = json.load(file)
                 pretty_print_json(json_data)
+            except:
+                print("%s is not a valid json file." % path)
 
 
 if __name__ == '__main__':
