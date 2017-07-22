29f02a6f7ee1a5f0bea6bb4172c2bd130d1d1fd2
antondemkin <antondemkin@yandex.ru>
2017-07-22 08:43:28 +0300

    Errors are much more informative now, code simplified even more.

--- pprint_json.py
+++ pprint_json.py
@@ -1,11 +1,12 @@
 import json
 import sys
-import os
+
 
 def load_json(filepath):
     with open(filepath) as file:
         json_data = json.load(file)
         return json_data
+
 
 def pretty_print_json(json_data):
     print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
@@ -14,14 +15,11 @@ def pretty_print_json(json_data):
 def main():
     if len(sys.argv) > 1:
         path = sys.argv[1]
-        if os.path.exists(path):
-            try:
-                json_data = load_json(path)
-                pretty_print_json(json_data)
-            except Exception as error:
-                print('Error: %s' % error)
-        else:
-            print('%s not found.' % path)
+        try:
+            json_data = load_json(path)
+            pretty_print_json(json_data)
+        except Exception as error:
+            print('Error: %s' % error)
     else:
         print('Usage: pprint_json.py [file]')
 
